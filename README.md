a291d765b91a833fb2f387dd886d96f3474f0d5b
Simon Moses Onazi <syflex360@gmail.com>
2023-12-30 12:35:42 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# website
+# juiceme
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00072F',
+        orange: '#FC6326'
+      }
+    },
+  },
+  plugins: [],
+}
